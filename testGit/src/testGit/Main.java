ec7e9fa97ab7eab95d10e71d73cf24e823c13620
yboutahar <yassineboutahar@gmail.com>
2016-04-24 11:57:38 +0200

    Update Main.java

--- testGit/src/testGit/Main.java
+++ testGit/src/testGit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("test");
-System.out.println("test 23");
+System.out.println("test 2333");
 	}
 
 }
